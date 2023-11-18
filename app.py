b2b2e3970c988ad124f5f738fe41856affbc5d07
TshepoHM <89998467+TshepoHM@users.noreply.github.com>
2023-11-18 13:29:55 +0000

    added jobs data

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    "id" : 1,
+    "title" : "Python Developer",
+    "location" : "Remote",
+    "salary" : "R7 000"
+  },
+  {
+    "id" : 2,
+    "title" : "Java Developer",
+    "location" : "Office",
+    "salary" : "R8 000"
+  }
+]
+
 @app.route("/")
 def hello():
-  return render_template("home.html")
+  return render_template("home.html", jobs = JOBS)
   
 
 
